eead0c1be1e9880b8a98309a60e6a55080c5e85a
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2024-12-22 15:29:45 +0530

    Time: 8 ms (28.25%), Space: 18.6 MB (46.52%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,35 +10,30 @@
  */
 class Solution {
 public:
-    ListNode* mergeLists(ListNode* head1, ListNode* head2){
-        ListNode* temp1 = head1;
-        ListNode* temp2 = head2;
-        ListNode* dummy = new ListNode(-1);
-        ListNode* cur = dummy;
-        while(temp1 && temp2){
-            if(temp1 -> val <= temp2->val){
-                cur->next = temp1;
-                cur = cur->next;
-                temp1 = temp1->next;
-            }
-            else{
-                cur->next = temp2;
-                cur= cur->next;
-                temp2 = temp2->next;
-            }
-        }
-        if(temp1) cur->next = temp1;
-        else cur->next = temp2;
-        return dummy ->next;
-    }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int k = lists.size();
-        if(k == 0) return nullptr;
-        if(k == 1) return lists[0];
-        ListNode* head = lists[0];
-        for(int i = 1; i<k; i++){
-            head = mergeLists(head, lists[i]);
+         int k = lists.size();
+         if(k==0){
+            return nullptr;
+         }
+         if(k == 1){
+            return lists[0];
+         }
+
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq;
+        for(int i = 0; i<k; i++){
+            if(lists[i]) pq.push({lists[i]->val, lists[i]});
         }
-        return head;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        while(!pq.empty()){
+            auto top = pq.top();
+            pq.pop();
+            ListNode* cur = top.second;
+            temp->next = cur;
+            temp = temp->next;
+            if(cur->next) pq.push({cur->next->val, cur->next});
+        }
+        return dummy->next;
+
     }
 };
