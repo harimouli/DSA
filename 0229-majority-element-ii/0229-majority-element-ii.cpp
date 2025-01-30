bc7973a8c0c40cb930dfe41a8d22709ca21f1fce
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2025-01-30 21:34:05 +0530

    Time: 0 ms (100%), Space: 19.8 MB (60.8%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,22 +1,40 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-            int n = nums.size();
-            if(n == 1){
-                return {nums[0]};
+        int n = nums.size();
+        int L1 = INT_MIN;
+        int L2 = INT_MIN;
+        int m1 = 0;
+        int m2 = 0;
+        for(int i = 0; i<n; i++){
+            if(m1 == 0 && nums[i] != L2 ){
+                L1 = nums[i];
+                m1 += 1;
             }
-            vector<int> ans;
-            unordered_map<int, int> mp;
-
-            for(int i = 0; i<n; i++){
-                mp[nums[i]] += 1;
+            else if(m2 == 0 && nums[i] != L1){
+                L2 = nums[i];
+                m2 +=1;
             }
-            for(auto &pair : mp){
-                if(pair.second > (n / 3)){
-                    ans.push_back(pair.first);
-                }
+            else if(L1 == nums[i]){
+                m1 += 1;
             }
-            return ans;
-
+            else if(L2 == nums[i]){
+                m2 += 1;
+            }
+            else{
+                m1 -=1;
+                m2 -= 1;
+            }
+        }
+        int count1 = 0;
+        int count2 = 0;
+        for(int i = 0; i<n; i++){
+            if(nums[i] == L1) count1 += 1;
+            if(nums[i] == L2) count2 += 1;
+        }
+        vector<int> ans;
+        if(count1 > n / 3) ans.push_back(L1);
+        if(count2 > n /3) ans.push_back(L2);
+        return ans;
     }
 };
