8c9deae2d14a9dc1cd09a552fc7eb1b2aa555400
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2025-03-19 20:09:57 +0530

    Time: 8 ms (48.85%), Space: 139.9 MB (74.5%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -7,9 +7,9 @@ public:
         for(int i = 0; i<=n-3; i++){
             if(nums[i] == 0){
                 operations++;
-                nums[i] = 1 - nums[i];
-                nums[i+1] = 1 - nums[i+1];
-                nums[i+2] = 1 - nums[i+2];
+                nums[i] ^=1;
+                nums[i+1] ^= 1;
+                nums[i+2] ^= 1;
             }
         }
 
