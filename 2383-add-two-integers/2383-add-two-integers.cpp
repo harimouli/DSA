8a886e8eed45ccd1c6628062338dc5129424793a
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2025-03-28 10:30:51 +0530

    Time: 0 ms (100%), Space: 8 MB (66.7%) - LeetHub

--- 2383-add-two-integers/2383-add-two-integers.cpp
+++ 2383-add-two-integers/2383-add-two-integers.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int sum(int num1, int num2) {
-        return num1 + num2;
+        
+        
+        int result = 0;
+        while(num2 != 0){
+                result = num1 ^ num2;
+                int carry = (num1 & num2) << 1;
+                num1 = result;
+                num2 = carry;
+        }
+        return num1;
     }
 };
