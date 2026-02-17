197c727da30fab63b156e4ef9648497eb75c3c40
MUTHYALA HARI MOULI <123249040+harimouli@users.noreply.github.com>
2026-02-17 06:27:49 +0530

    Time: 55 ms (68%), Space: 14.1 MB (78.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-
-    int longestSub(int index, int prevIndex, vector<vector<int>> &dp, vector<int> &nums){
-        int n = nums.size();
-
-        if(index == n) return 0;
-
-        if(dp[index][prevIndex + 1] != -1) return dp[index][prevIndex + 1];
-        int notPick = longestSub(index + 1, prevIndex, dp, nums);
-        int pick = 0;
-
-        if(prevIndex == -1  || nums[index] > nums[prevIndex]){
-            pick = 1 + longestSub(index + 1, index, dp, nums);
-        }
-
-        return dp[index][prevIndex + 1] = max(pick , notPick);
-
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n + 1, vector<int>(n + 1, -1));
-        return longestSub(0, -1, dp, nums);
+
+        int len = 1;
+
+        vector<int> temp;
+        temp.push_back(nums[0]);
+
+        for(int i = 1; i<n; i++){
+
+            if(nums[i] > temp.back()){
+                temp.push_back(nums[i]);
+                len++;
+            }else{
+                int ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+                temp[ind] = nums[i];
+                cout<<ind<<endl;
+            }
+        }
+        return len;
     }
 };
