340459f4a9f984683560d4c1a8d8bcc6b7d4b84f
MUTHYALA HARI MOULI <harimoulimuthyala@gmail.com>
2026-06-01 19:10:44 +0530

    Time: 0 ms (100%), Space: 9.8 MB (10.55%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>tri(numRows);
+        
+        
+        
+        vector<vector<int>> tri(numRows);
 
 
-        for(int i = 0; i<numRows; i++){
-            tri[i].resize(i+1);
-            for(int j = 0; j<i+1; j++){
+        for(int i = 0; i < numRows; i++){
+            tri[i].resize(i + 1);
+
+            for(int j = 0; j <= i; j++){
                 if(j == 0 || j == i) tri[i][j] = 1;
                 else{
-                    tri[i][j] = tri[i-1][j] + tri[i-1][j-1];
+                    tri[i][j] = tri[i-1][j] + tri[i - 1][j -1];
                 }
             }
         }
         return tri;
-           
+
     }
 };
