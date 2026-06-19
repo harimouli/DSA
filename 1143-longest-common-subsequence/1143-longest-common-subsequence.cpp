d4ce4db14fa109228d5776d464767bc5146a7245
MUTHYALA HARI MOULI <harimoulimuthyala@gmail.com>
2026-06-19 16:30:56 +0530

    Time: 24 ms (71.55%), Space: 27.4 MB (68.24%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-
-    
-
-    int maxCommSubsequence(int ind1, int ind2, string &text1, string &text2, vector<vector<int>> &dp){
+    int maxCommSubsequence(int ind1, int ind2, string& text1, string& text2,
+                           vector<vector<int>>& dp) {
         int m = text1.size();
         int n = text2.size();
-        if(ind1 == m || ind2 == n) return 0;
 
-        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
-        if(text1[ind1] == text2[ind2]){
-            return 1 + maxCommSubsequence(ind1 + 1, ind2 + 1, text1, text2, dp);
+        for (int ind1 = m-1; ind1 >= 0; ind1--) {
+
+            for (int ind2 = n-1; ind2 >= 0; ind2--) {
+
+                if (text1[ind1] == text2[ind2]) {
+                    dp[ind1][ind2] = 1 + dp[ind1 + 1][ind2 +  1];
+                } else {
+                    int f1 = dp[ind1 + 1][ind2];
+
+                    int f2 = dp[ind1][ind2 + 1];
+
+                    dp[ind1][ind2] = max(f1, f2);
+                }
+            }
         }
-        int f1 = 0;
-        int f2 = 0;
-        if( ind1 + 1 < m) f1 = maxCommSubsequence(ind1 + 1, ind2, text1, text2, dp);
-        if(ind2 + 1 < n) f2 = maxCommSubsequence(ind1, ind2 +  1, text1, text2, dp);
-
-        return dp[ind1][ind2] = max(f1, f2); 
+        return dp[0][0];
     }
     int longestCommonSubsequence(string text1, string text2) {
-        
+
         int m = text1.size();
         int n = text2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+      
+
         return maxCommSubsequence(0, 0, text1, text2, dp);
     }
 };
