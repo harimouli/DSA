8071638c761ea798c3929402aab55afe55c97317
MUTHYALA HARI MOULI <harimoulimuthyala@gmail.com>
2026-06-27 03:51:44 +0530

    Time: 3 ms (89.19%), Space: 23.9 MB (31.09%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         
+
         sort(intervals.begin(), intervals.end());
 
         int n = intervals.size();
 
-
         vector<vector<int>> res;
+        
+        res.push_back(intervals[0]);
+        for(int i = 1; i < n; i++){
 
-        for(int i = 0; i<n; i++){
-            if(res.empty() || res.back()[1] < intervals[i][0]){
-                res.push_back(intervals[i]);
+            if(res.back()[1] >= intervals[i][0]){
+                
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
             }
             else{
-                res.back()[1] = max(intervals[i][1], res.back()[1]);
+                res.push_back(intervals[i]);
             }
+
         }
         return res;
-        
     }
 };
