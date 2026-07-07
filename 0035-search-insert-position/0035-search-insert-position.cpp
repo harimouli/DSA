378d5dadc5586a1635a96dae4c2035828d90b9d3
MUTHYALA HARI MOULI <harimoulimuthyala@gmail.com>
2026-07-07 11:12:45 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.02%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,10 +4,20 @@ public:
 
 
 
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] >= target) return i;
-        }
+        int low = 0; 
+        int high = nums.size() - 1;
 
-        return nums.size();
+        int res = nums.size();
+        while(low <= high){
+                int mid = (high + low) / 2;
+
+                if(nums[mid]  >= target){
+                    res = mid;
+                    high = mid - 1;
+                }
+                else  low = mid + 1;
+               
+        }
+        return res;
     }
 };
