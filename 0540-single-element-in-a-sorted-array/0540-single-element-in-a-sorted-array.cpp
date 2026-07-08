0eb23a70ce4170d154a6946f67750a6fadf9029f
MUTHYALA HARI MOULI <harimoulimuthyala@gmail.com>
2026-07-08 15:55:01 +0530

    Time: 0 ms (100%), Space: 26.2 MB (68.56%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,10 +3,27 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         
 
-        int xorA = nums[0];
-        for(int i = 1; i<nums.size(); i++){
-            xorA ^= nums[i];
+        int n = nums.size();
+
+        if(n == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+
+        int low = 1;
+        int high = n - 2;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]){
+                return nums[mid];
+            }else if(mid % 2 == 1 && nums[mid] == nums[mid-1]  || mid % 2 == 0 && nums[mid] == nums[mid+1]){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
         }
-        return xorA;
+        return -1;
+
     }
 };
